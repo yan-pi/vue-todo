b27735d4bc3f9376e6c914213a40ed56d1885d09
Yan Fernandes <yanfernandes404@gmail.com>
2023-07-27 17:16:33 -0300

    Feat: implementa Sass

--- src/App.vue
+++ src/App.vue
@@ -116,4 +116,233 @@ onMounted(() => {
   </main>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+
+
+input:not([type="radio"]):not([type="checkbox"]), button {
+	appearance: none;
+	border: none;
+	outline: none;
+	background: none;
+	cursor: initial;
+}
+
+body {
+	background: $light;
+	color: $dark;
+  
+  section {
+	margin-top: 2rem;
+	margin-bottom: 2rem;
+	padding-left: 1.5rem;
+	padding-right: 1.5em;
+  }
+
+  h3 {
+	  color: $dark;
+	  font-size: 1rem;
+	  font-weight: 400;
+	  margin-bottom: 0.5rem;
+  }
+
+  h4 {
+	  color: $grey;
+	  font-size: 0.875rem;
+	  font-weight: 700;
+  }
+}
+
+
+.greeting {
+	display: flex;
+  
+  .title input {
+	margin-left: 0.5rem;
+	flex: 1 1 0%;
+	min-width: 0;
+
+}
+
+  input {
+	  color: $dark;
+	  font-size: 1.5rem;
+	  font-weight: 700;
+  }
+}
+
+
+
+.create-todo {
+  input[type="text"] {
+  	display: block;
+  	width: 100%;
+  	font-size: 1.125rem;
+  	padding: 1rem 1.5rem;
+  	color: $dark;
+  	background-color: #FFF;
+  	border-radius: 0.5rem;
+	  box-shadow: $shadow;
+	  margin-bottom: 1.5rem;
+  }
+
+
+  .options {
+	  display: grid;
+	  grid-template-columns: repeat(2, 1fr);
+	  grid-gap: 1rem;
+	  margin-bottom: 1.5rem;
+  }
+
+  label{
+    display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  justify-content: center;
+	  padding: 1.5rem;
+	  background-color: #FFF;
+	  border-radius: 0.5rem;
+	  box-shadow: $shadow;
+	  cursor: pointer;
+  }
+}
+
+
+input[type="radio"],
+input[type="checkbox"] {
+	display: none;
+}
+
+.bubble {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 20px;
+	height: 20px;
+	border-radius: 50%;
+	border: 2px solid $business;
+	box-shadow: $business-glow;
+
+  &:after {
+	  content: "";
+	  display: block;
+	  opacity: 0;
+	  width: 0px;
+	  height: 0px;
+	  background-color: $business;
+	  box-shadow: $business-glow;
+	  border-radius: 50%;
+	  transition: 0.2s ease-in-out;
+  }
+
+  .business{
+    border-color: $business;
+    box-shadow: $business-glow;
+  }
+
+  .personal {
+	  border-color: $personal;
+	  box-shadow: $personal-glow;
+
+    &:after {
+	    background-color: $personal;
+	    box-shadow: $personal-glow;
+    }
+  }
+
+  input:checked ~ .bubble::after {
+    color: aquamarine;
+  	width: 10px;
+  	height: 10px;
+  	opacity: 1;
+  }
+}
+
+.create-todo {
+  .options label div {
+	  color: $dark;
+	  font-size: 1.125rem;
+	  margin-top: 1rem;
+  }
+
+
+  input[type="submit"] {
+	  display: block;
+	  width: 100%;
+	  font-size: 1.125rem;
+	  padding: 1rem 1.5rem;
+  	color: #FFF;
+  	background-color: $primary;
+  	border-radius: 0.5rem;
+  	box-shadow: $personal-glow;
+  	cursor: pointer;
+  	transition: 0.2s ease-in-out;
+
+   &:hover {
+  	  opacity: 0.75;
+     }
+  }
+}
+
+
+.todo-list .list {
+	margin: 1rem 0;
+} 
+
+.todo-list .todo-item {
+	display: flex;
+	align-items: center;
+	background-color: #FFF;
+	padding: 1rem;
+	border-radius: 0.5rem;
+	box-shadow: $shadow;
+	margin-bottom: 1rem;
+}
+
+.todo-item label {
+	display: block;
+	margin-right: 1rem;
+	cursor: pointer;
+}
+
+.todo-item .todo-content {
+	flex: 1 1 0%;
+}
+
+.todo-item .todo-content input {
+	color: $dark;
+	font-size: 1.125rem;
+}
+
+.todo-item .actions {
+	display: flex;
+	align-items: center;
+}
+
+.todo-item .actions button {
+	display: block;
+	padding: 0.5rem;
+	border-radius: 0.25rem;
+	color: #FFF;
+	cursor: pointer;
+	transition: 0.2s ease-in-out;
+}
+
+.todo-item .actions button:hover {
+	opacity: 0.75;
+}
+
+.todo-item .actions .edit {
+	margin-right: 0.5rem;
+	background-color: $primary;
+}
+
+.todo-item .actions .delete {
+	background-color: $danger;
+}
+
+.todo-item.done .todo-content input {
+	text-decoration: line-through;
+	color: $grey;
+}
+
+</style>
